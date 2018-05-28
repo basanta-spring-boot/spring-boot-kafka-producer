be15c7055d8e05cb06330a01253897328757836b
Basant kumar Hota <basahota@users.noreply.github.com>
2018-05-28 13:51:12 +0530

    #code

---
+++ src/main/java/com/spring/kafka/api/SpringKafkaProducerApplication.java
@@ -0,0 +1,12 @@
+package com.spring.kafka.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaProducerApplication.class, args);
+	}
+}
